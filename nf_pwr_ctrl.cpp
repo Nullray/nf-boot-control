3c0af5eb23321d582242ceda31cc43e1f6212d84
asdw391 <43629064+asdw391@users.noreply.github.com>
2021-08-25 13:47:55 +0800

    test power match

--- nf_pwr_ctrl.cpp
+++ nf_pwr_ctrl.cpp
@@ -210,8 +210,11 @@ namespace nf_pwr_ctrl
       });
     static auto powermatch = sdbusplus::bus::match::match(
         *conn,
-        "type='signal',member=CriticalHighAlarmAsserted",
+        "type='signal',member='PropertiesChanged',"
+        "path_namespace='/xyz/openbmc_project/sensors/power',"
+        "arg0='xyz.openbmc_project.Sensor.Threshold.Critical'"
         [](sdbusplus::message::message& m) {
+            std::cerr << "[PWCTL_log]match a high Alarm Assert"<<m.get_path()<<std::endl;
             CriticalhighPowerControl(m);
         }
     );
