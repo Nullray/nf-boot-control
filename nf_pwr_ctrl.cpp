ddbb3ac677a9925c675ee5b1985a6e610cfc02c3
asdw391 <43629064+asdw391@users.noreply.github.com>
2021-08-26 09:16:15 +0800

    when NF card power sensor reached high value, it will raise critical message. the new code will capture this warning message and send message to dbus , turn off the power supply.

--- nf_pwr_ctrl.cpp
+++ nf_pwr_ctrl.cpp
@@ -16,77 +16,164 @@
 
 namespace nf_pwr_ctrl
 {
-static boost::asio::io_service io;
-static std::shared_ptr<sdbusplus::asio::connection> conn;
-static std::shared_ptr<sdbusplus::asio::dbus_interface> nfBladeIface[MAX_NF_CARD_NUMS];
+  static boost::asio::io_service io;
+  static std::shared_ptr<sdbusplus::asio::connection> conn;
+  static std::shared_ptr<sdbusplus::asio::dbus_interface> nfBladeIface[MAX_NF_CARD_NUMS];
 
-static constexpr const char* nfPowerService = "xyz.openbmc_project.nf.power.manager";
-static constexpr const char* nfPowerIface = "xyz.openbmc_project.NF.Blade.Power";
-static std::string nfPowerPath = "/xyz/openbmc_project/control/nf/";
-static std::string nfBladePath[MAX_NF_CARD_NUMS];
+  static constexpr const char* nfPowerService = "xyz.openbmc_project.nf.power.manager";
+  static constexpr const char* nfPowerIface = "xyz.openbmc_project.NF.Blade.Power";
+  static std::string nfPowerPath = "/xyz/openbmc_project/control/nf/";
+  static std::string nfBladePath[MAX_NF_CARD_NUMS];
 
-static std::string nfpwrTemplate = "slot_x_pwr";
-static std::string nfpwrOut[MAX_NF_CARD_NUMS];
+  static std::string nfpwrTemplate = "slot_x_pwr";
+  static std::string nfpwrOut[MAX_NF_CARD_NUMS];
 
-static std::string nfprsntTemplate = "slot_x_prsnt";
-static std::string nfprsntIn[MAX_NF_CARD_NUMS];
+  static std::string nfprsntTemplate = "slot_x_prsnt";
+  static std::string nfprsntIn[MAX_NF_CARD_NUMS];
 
-static std::string nfresetnTemplate = "slot_x_resetn";
-static std::string nfresetnOut[MAX_NF_CARD_NUMS];
+  static std::string nfresetnTemplate = "slot_x_resetn";
+  static std::string nfresetnOut[MAX_NF_CARD_NUMS];
 
-static std::string nfintfselTemplate = "nf_uart_jtag_x_sel";
-static std::string nfintfselOut[MAX_NF_CARD_NUMS];
+  static std::string nfintfselTemplate = "nf_uart_jtag_x_sel";
+  static std::string nfintfselOut[MAX_NF_CARD_NUMS];
 
-static bool GPIOLine(const std::string& name, const int out,
-                          gpiod::line& gpioLine, int& value)
-{
-  // Find the GPIO line
-  gpioLine = gpiod::find_line(name);
-  if (!gpioLine)
+  static bool GPIOLine(const std::string& name, const int out,
+    gpiod::line& gpioLine, int& value)
   {
-    std::cerr << "Failed to find the " << name << " line.\n";
-    return false;
-  }
+  // Find the GPIO line
+    gpioLine = gpiod::find_line(name);
+    if (!gpioLine)
+    {
+      std::cerr << "Failed to find the " << name << " line.\n";
+      return false;
+    }
 
   // Request GPIO line as input/output
-  try
-  {
-    if(out)
-      gpioLine.request({__FUNCTION__, gpiod::line_request::DIRECTION_OUTPUT},
-                         value);
-    
-    else {
-      gpioLine.request({__FUNCTION__, gpiod::line_request::DIRECTION_INPUT});
-      value = gpioLine.get_value();
+    try
+    {
+      if(out)
+        gpioLine.request({__FUNCTION__, gpiod::line_request::DIRECTION_OUTPUT},
+         value);
+      
+      else {
+        gpioLine.request({__FUNCTION__, gpiod::line_request::DIRECTION_INPUT});
+        value = gpioLine.get_value();
+      }
     }
+    catch (std::exception&)
+    {
+      std::cerr << "Failed to request " << name << "\n";
+      return false;
+    }
+
+    return true;
   }
-  catch (std::exception&)
+
+  static void ObjPathtoLineName(std::string &obj_path, std::string &line_name)
   {
-    std::cerr << "Failed to request " << name << "\n";
-    return false;
+    size_t pos = 0;
+    std::string token;    
+    std::string delimiter = "/";
+    std::string blade_fix = "blade";
+
+    while((pos = obj_path.find(delimiter)) != std::string::npos) {
+      token = obj_path.substr(0, pos);
+      obj_path.erase(0, pos + delimiter.length());
+    }
+    line_name = "slot_" + obj_path.substr(blade_fix.length(), 2) + line_name;
+    std::cerr << "line_name: " << line_name << "\n";
   }
 
-  return true;
-}
+  static void CriticalhighPowerControl(sdbusplus::message::message& msg) {
+      std::string interfaceName;
+      boost::container::flat_map<std::string,
+          std::variant<bool, std::string>> propertiesChanged;
+      try
+      {
+          msg.read(interfaceName,propertiesChanged);
+      }
+      catch (sdbusplus::exception_t&)
+      {
+          std::cerr << "[PWCTL_log]error reading message"<<std::endl;
+          return;
+      }
+      // state: interface property state
+      bool state = std::get<bool>(propertiesChanged.begin()->second);
+      // alarm high or alarm low
+      bool highAlarm = (propertiesChanged.begin()->first == "CriticalAlarmHigh");
+      if (state == false || highAlarm == false)return;
 
-static void ObjPathtoLineName(std::string &obj_path, std::string &line_name)
-{
-  size_t pos = 0;
-  std::string token;    
-  std::string delimiter = "/";
-  std::string blade_fix = "blade";
+      static std::string pathFront = "/xyz/openbmc_project/sensors/power/NF";
+      static std::string pathEnd = "_power";
+      
+      std::string path = std::string(msg.get_path());
+      //regex path to get the name of the sensor who send the critical message
+      if (path.substr(0, pathFront.length()) == pathFront) {
+          path.erase(path.length() - pathEnd.length());     
+          std::string boardIndex = path.substr(std::string(pathFront).length(), 2);
+          std::cerr << "[PWCTL_log] boardIndex should be a integer : " << boardIndex<<std::endl;
 
-  while((pos = obj_path.find(delimiter)) != std::string::npos) {
-    token = obj_path.substr(0, pos);
-    obj_path.erase(0, pos + delimiter.length());
+          // this following code copy from bmcweb /redfish-core/lib/systems.hpp.
+          // set the  dbus property state to power.off.
+          {
+              std::string command = "Power.Off";
+              std::string systemId_path;
+              systemId_path.assign("nf/blade"+boardIndex);
+
+              conn->async_method_call(
+                  [systemId_path](
+                      const boost::system::error_code ec,
+                      const std::variant<std::string>& property)
+                  {
+                      if (ec)
+                      {
+                          std::cerr << "[PWCTL_log] Critical Warning could not find Dbus power control";
+                          return;
+                      }
+
+                      else
+                      {
+                          std::string status;
+                          const std::string* value =
+                              std::get_if<std::string>(&property);
+
+                          status.assign(*value);
+
+                          if (status == "false")
+                          {
+                              std::cerr << "[PWCTL_log] internal error.";
+                              return;
+                          }
+                      }
+
+                      // send SET command to D-Bus
+                      conn->async_method_call(
+                          [](const boost::system::error_code ec2)
+                          {
+                              if (ec2)
+                              {
+                                  std::cerr << "[PWCTL_log] send stop message failed";
+                                  return;
+                              }
+                          },
+                          "xyz.openbmc_project.nf.power.manager",
+                              "/xyz/openbmc_project/control/" + systemId_path,
+                              "org.freedesktop.DBus.Properties", "Set",
+                              "xyz.openbmc_project.NF.Blade.Power",
+                              "Asserted",
+                              std::variant<std::string>{"Power.Off"});
+                  },
+                  "xyz.openbmc_project.nf.power.manager",
+                      "/xyz/openbmc_project/control/" + systemId_path,
+                      "org.freedesktop.DBus.Properties", "Get",
+                      "xyz.openbmc_project.NF.Blade.Power", "Attached");
+          }
+      }
   }
-  line_name = "slot_" + obj_path.substr(blade_fix.length(), 2) + line_name;
-  std::cerr << "line_name: " << line_name << "\n";
-}
 
-static void PowerControl()
-{
-  static auto match = sdbusplus::bus::match::match(
+  static void PowerControl()
+  {
+    static auto match = sdbusplus::bus::match::match(
       *conn,
       "type='signal',member='PropertiesChanged', "
       "interface='org.freedesktop.DBus.Properties', "
@@ -94,8 +181,8 @@ static void PowerControl()
       [](sdbusplus::message::message& m) {
         std::string intfName;
         boost::container::flat_map<std::string, 
-             std::variant<bool, std::string>> propertiesChanged;
-             
+        std::variant<bool, std::string>> propertiesChanged;
+        
         m.read(intfName, propertiesChanged);
         std::string obj_path;
         obj_path = m.get_path();
@@ -127,19 +214,28 @@ static void PowerControl()
           return;
         }
       });
-}
+    static auto powermatch = sdbusplus::bus::match::match(
+        *conn,
+        "type='signal',member='PropertiesChanged',"
+        "path_namespace='/xyz/openbmc_project/sensors/power',"
+        "arg0='xyz.openbmc_project.Sensor.Threshold.Critical'",
+        [](sdbusplus::message::message& m) {
+            CriticalhighPowerControl(m);
+        }
+    );
+  }
 }
 
 int main(int argc, char* argv[])
 {
   std::cerr << "Start NF card power control service...\n";
   nf_pwr_ctrl::conn = 
-    std::make_shared<sdbusplus::asio::connection>(nf_pwr_ctrl::io);
+  std::make_shared<sdbusplus::asio::connection>(nf_pwr_ctrl::io);
   
   nf_pwr_ctrl::conn->request_name(nf_pwr_ctrl::nfPowerService);
   
   sdbusplus::asio::object_server server =
-    sdbusplus::asio::object_server(nf_pwr_ctrl::conn);
+  sdbusplus::asio::object_server(nf_pwr_ctrl::conn);
 
   std::string gpio_name;
   int i;
@@ -181,68 +277,68 @@ int main(int argc, char* argv[])
     
     /** set nf blade dbus path */
     nf_pwr_ctrl::nfBladePath[i] = 
-      nf_pwr_ctrl::nfPowerPath + "blade" + std::to_string(i);
+    nf_pwr_ctrl::nfPowerPath + "blade" + std::to_string(i);
 
     current_blade.assign(nf_pwr_ctrl::nfBladePath[i].c_str());
     
     /** setup nf/blade<x> dbus object */
     nf_pwr_ctrl::nfBladeIface[i] = 
-      server.add_interface(
-          nf_pwr_ctrl::nfBladePath[i], nf_pwr_ctrl::nfPowerIface);
+    server.add_interface(
+      nf_pwr_ctrl::nfBladePath[i], nf_pwr_ctrl::nfPowerIface);
     
     /** add *Asserted* dbus property to nf/blade<x>/ dbus object */
     nf_pwr_ctrl::nfBladeIface[i]->register_property("Asserted", 
-        std::string("Power.Off"), 
-        sdbusplus::asio::PropertyPermission::readWrite);
+      std::string("Power.Off"), 
+      sdbusplus::asio::PropertyPermission::readWrite);
     
     /** add *Reset* dbus property to nf/blade<x>/ dbus object */
     nf_pwr_ctrl::nfBladeIface[i]->register_property("WarmReset", 
-        std::string("Done"), 
+      std::string("Done"), 
         //custom set
-        [current_blade](const std::string& req, const std::string& property) {
-          std::string line_name = "_resetn";
-          std::string obj_path;
-          obj_path.assign(current_blade.c_str());
+      [current_blade](const std::string& req, const std::string& property) {
+        std::string line_name = "_resetn";
+        std::string obj_path;
+        obj_path.assign(current_blade.c_str());
 
-          nf_pwr_ctrl::ObjPathtoLineName(obj_path, line_name);
+        nf_pwr_ctrl::ObjPathtoLineName(obj_path, line_name);
 
-          if (req == "Force")
-          {
+        if (req == "Force")
+        {
             /* reset output */
-            gpiod::line line;
-            int value = 0;
-            nf_pwr_ctrl::GPIOLine(line_name, GPIO_OUT, line, value);
+          gpiod::line line;
+          int value = 0;
+          nf_pwr_ctrl::GPIOLine(line_name, GPIO_OUT, line, value);
 
-            usleep(5000);
+          usleep(5000);
 
-            value = 1;
-            nf_pwr_ctrl::GPIOLine(line_name, GPIO_OUT, line, value);
+          value = 1;
+          nf_pwr_ctrl::GPIOLine(line_name, GPIO_OUT, line, value);
 
-            line.reset();
-          }
+          line.reset();
+        }
           /* There is no need to update D-Bus attribute value */
-          return 1;
-        });
+        return 1;
+      });
     
     /** add *Attached* dbus property to nf/blade<x>/ dbus object */
     nf_pwr_ctrl::nfBladeIface[i]->register_property_r("Attached", 
-        std::string("false"), 
-        sdbusplus::vtable::property_::none,
+      std::string("false"), 
+      sdbusplus::vtable::property_::none,
         //custom get
-        [current_blade](const std::string& property) {
-          std::string line_name = "_prsnt";
-          std::string obj_path;
-          obj_path.assign(current_blade.c_str());
-          
-          nf_pwr_ctrl::ObjPathtoLineName(obj_path, line_name);
+      [current_blade](const std::string& property) {
+        std::string line_name = "_prsnt";
+        std::string obj_path;
+        obj_path.assign(current_blade.c_str());
+        
+        nf_pwr_ctrl::ObjPathtoLineName(obj_path, line_name);
 
           /* read GPIO line */
-          gpiod::line line;
-          int value;
-          nf_pwr_ctrl::GPIOLine(line_name, GPIO_IN, line, value);
-          line.reset();
-          
-          return (value ? "false" : "true");
+        gpiod::line line;
+        int value;
+        nf_pwr_ctrl::GPIOLine(line_name, GPIO_IN, line, value);
+        line.reset();
+        
+        return (value ? "false" : "true");
       });
     
     nf_pwr_ctrl::nfBladeIface[i]->initialize();
@@ -258,22 +354,22 @@ int main(int argc, char* argv[])
 
     // set output GPIO with initial value 1
     if (!nf_pwr_ctrl::GPIOLine(
-          nf_pwr_ctrl::nfpwrOut[i], GPIO_OUT, gpioLine, value))
+      nf_pwr_ctrl::nfpwrOut[i], GPIO_OUT, gpioLine, value))
       return -1;
     
     if (!nf_pwr_ctrl::GPIOLine(
-          nf_pwr_ctrl::nfresetnOut[i], GPIO_OUT, gpioLine, value))
+      nf_pwr_ctrl::nfresetnOut[i], GPIO_OUT, gpioLine, value))
       return -1;
 
     value = 0;
     // set UART and JTAG of each NF card to BH motherboard as default
     if (!nf_pwr_ctrl::GPIOLine(
-          nf_pwr_ctrl::nfintfselOut[i], GPIO_OUT, gpioLine, value))
+      nf_pwr_ctrl::nfintfselOut[i], GPIO_OUT, gpioLine, value))
       return -1;
     
     // set input GPIO
     if (!nf_pwr_ctrl::GPIOLine(
-          nf_pwr_ctrl::nfprsntIn[i], GPIO_IN, gpioLine, value))
+      nf_pwr_ctrl::nfprsntIn[i], GPIO_IN, gpioLine, value))
       return -1;
   }
   
